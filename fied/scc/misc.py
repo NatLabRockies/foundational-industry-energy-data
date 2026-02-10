d0a6e119b57440a6b7ac72a7b8cd0a327e0527cb
Gui Castelao <guilherme@castelao.net>
2026-02-09 21:29:56 -0700

    Adding match_fuel_type like ghgrp_2tier

--- fied/scc/misc.py
+++ fied/scc/misc.py
@@ -8,3 +8,37 @@ def load_scc_fuel_types():
     yaml_path = files('fied.scc').joinpath('fuel_type_standardization.yaml')
     with yaml_path.open('r') as f:
         return yaml.safe_load(f)
+
+
+def match_fuel_type(ft):
+    """
+    Match fuel type to an entry in all fuel types yaml. Returns
+    standardized level 1 and level 2 fuel types.
+
+    Parameters
+    ----------
+    ft : str
+        Fuel type
+
+    Returns
+    -------
+    ft1, ft2 : str
+        Standardized level 1 and level 2 fuel types.
+
+    Raises
+    ------
+    KeyError
+        If the fuel type is not included in the all fuel types yaml.
+    """
+
+    try:
+        ft1 = load_scc_fuel_types()[ft]['fuelTypeLv1']
+        ft2 = load_scc_fuel_types()[ft]['fuelTypeLv2']
+
+    except KeyError as e:
+        logging.error(f"{e}, fuel type: {ft}")
+
+        return "Other", ft
+
+    else:
+        return ft1, ft2
