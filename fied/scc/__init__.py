5683dd270cd9338c1bdaeccedc2be88f722ae238
Gui Castelao <guilherme@castelao.net>
2026-03-28 19:38:43 -0600

    feat: Exposing scc_unit_and_fuel_types() from scc module
    It looks like it is the only component that we have to expose to the
    rest of the library.

--- fied/scc/__init__.py
+++ fied/scc/__init__.py
@@ -2,3 +2,8 @@
 
 Methods to download and apply EPA's Source Classification Codes for characterizing units.
 """
+import polars as pl
+
+from .core import scc_unit_and_fuel_types
+
+__all__ = ["scc_unit_and_fuel_types"]
